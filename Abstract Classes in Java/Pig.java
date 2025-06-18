2951d377624e160098aa5527934966bb1e84de88
TshegofatsoT <49747773+TshegofatsoT@users.noreply.github.com>
2025-06-18 15:55:32 +0200

    Create Pig.java

---
+++ Abstract Classes in Java/Pig.java
@@ -0,0 +1,10 @@
+package com.mycompany.abstract_animal;
+
+class Pig extends Animal {
+
+    @Override
+    public void amimalSound() {
+        System.out.println("The pig said wee wee.");
+    }
+
+}
