9aa3af1af865d14dbd3004185fe6f7a8825a3ca5
TshegofatsoT <49747773+TshegofatsoT@users.noreply.github.com>
2025-06-18 15:54:54 +0200

    Create Animal.java

---
+++ Abstract Classes in Java/Animal.java
@@ -0,0 +1,10 @@
+package com.mycompany.abstract_animal;
+
+abstract class Animal {
+
+    public abstract void amimalSound();
+
+    public void sleep() {
+        System.out.println("zzz");
+    }
+}
